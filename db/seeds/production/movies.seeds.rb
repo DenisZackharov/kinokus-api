92e99f9291a02a96224f033905aad610788e2506
Denis Zakharov <dizakharov@domclick.ru>
2024-11-19 16:40:00 +0300

    update seeds

--- db/seeds/production/movies.seeds.rb
+++ db/seeds/production/movies.seeds.rb
@@ -4,15 +4,12 @@ green_mile = Movie.find_or_create_by!(name: "Green Mile", description: "The Gree
 kill_bill = Movie.find_or_create_by!(name: "Kill Bill", description: "Quentin Tarantino film.")
 pulp_fiction = Movie.find_or_create_by!(name: "Pulp Fiction", description: "Most popular Quentin Tarantino film.")
 
-pulp_fiction_url = "https://m.media-amazon.com/images/M/MV5BYTViYTE3ZGQtNDBlMC00ZTAyLTkyODMtZGRiZDg0MjA2YThkXkEyXkFqcGc@._V1_.jpg"
-pulp_fiction_file = URI.parse(pulp_fiction_url).open
+test_url = "https://26601fec-9278-484e-9320-fb51eb6a5c97.selstorage.ru/photo_2024-11-18_18-03-47.jpg"
+pulp_fiction_file = URI.parse(test_url).open
 
-green_mile_url = "https://m.media-amazon.com/images/M/MV5BMTUxMzQyNjA5MF5BMl5BanBnXkFtZTYwOTU2NTY3._V1_FMjpg_UX1000_.jpg"
-green_mile_file = URI.parse(green_mile_url).open
+green_mile_file = URI.parse(test_url).open
 
-kill_bill_url = "https://m.media-amazon.com/images/M/MV5BZmMyYzJlZmYtY2I3NC00NjAyLTkyZWItZjdjZDI1YTYyYTEwXkEyXkFqcGc@._V1_.jpg"
-kill_bill_file = URI.parse(kill_bill_url).open
-
+kill_bill_file = URI.parse(test_url).open
 
 green_mile.poster.attach(io: green_mile_file, filename: "green_mile.jpg") unless green_mile.poster.attached?
 kill_bill.poster.attach(io: kill_bill_file, filename: "kill_bill.jpg") unless kill_bill.poster.attached?
